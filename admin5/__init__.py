b90823c5a984cbef8f661af99e41799f4f19aa34
Paul Everitt <paul@agendaless.com>
2014-12-14 13:49:39 -0500

    Remove unused files. Add the correct permission.

--- admin5/__init__.py
+++ admin5/__init__.py
@@ -4,4 +4,5 @@ def includeme(config):
     # Setup some routes to static-serve /admin5/
 
     # TODO Need a KarlAdmin permission on this
-    config.add_static_view(name='admin5', path='admin5:../dist')
+    config.add_static_view(name='admin5', path='admin5:../dist',
+                           permission='administer')
