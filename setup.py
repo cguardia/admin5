7ab8e9fd6156293482f0ea52cc7dbe3907d63765
Chris McDonough <chrism@plope.com>
2015-03-16 14:59:29 -0400

    changes suggested in email to paul

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 __version__ = '0.2'
 
 setup(
     version=__version__,
-    name='admin5'
+    name='admin5',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
 )
